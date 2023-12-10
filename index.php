5693bd88ad38ff3dcddd2ab2206cb38133dc5e75
PapriBiswas2023 <official.p2023@gmail.com>
2023-12-10 15:46:23 +0530

    gh

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Demo4</title>
+<link href="css/style_sheet.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+	<header style=" background-color: #F2F2F2; width: 100%; height:60px; bottom:0;">
+    	<img src="css/dac758d7606690d057a9cbba5f259154.jpg" style=" width:5%; height: 60px; padding-left: 20px;" />
+        <h2 style=" float:right; padding-right: 30px; padding-top:5px; font-family: Comic Sans MS; font-size:36px;">Magic world</h2>
+    </header>
+
+	<div class="loginpage">
+    	<h1>LOGIN</h1>
+        <form>
+        	<label for="uname"></label>
+            <input type="text" id="uname" name="uname" placeholder="USERNAME" />
+            <label for="pass"></label>
+            <input type="password" id="passwd" name="uname" placeholder="PASSWORD" />
+            <br />
+            <p class="F"><b>Forgot Password</b><p>
+            <p class="R">REMEMBER ME<p>
+            <input type="button" class="button" value="SIGN IN"/> 
+        </form>
+    </div>
+   <footer id="footer"  >
+  	 <!-- <p class="copyright" style=" text-align:right;">@copyright 2023-2024<br />Developed by Papri Biswas</p>-->
+    	<!--<p class="papri">Developed by Papri Biswas</p>-->
+    	<div class="logof">
+   			 <div class="logo1" style=" display:block;">
+             	 <p style=" padding-top:20px; padding-left: 15px; color:#FFF;   display:inline;"><b>Contact Info :</b></p><br />
+
+    			 <img src="css/facebook-logo.png"  style="width:3%; height:15px; align: left; padding-top: 27px; padding-left:10px;"/>
+                 <img src="css/insta.png"  style="width:3%; height:15px; align: left; padding-top: 27px;padding-left:10px;"/>
+                 <img src="css/twitter.jpg" style="width:3%; height:15px; align: left; padding-top: 27px; padding-left:10px;" />
+                  <img src="css/whatsapp-logo-png-hd-2.png" style="width:3%; height:15px; align: left; padding-top: 27px; padding-left:10px;" />
+  	             <p style=" color:#FFF; padding-top:10px; padding-right: 15px;  float:right; display:inline;">@copyright 2023-2024<br />Developed by Papri Biswas</p>
+   		 	</div>
+        <!-- <div class="logo2">
+         	<img src="css/insta.png" />
+         </div>
+         <div class="logo3">
+         <img src="css/twitter.jpg" />
+         </div>
+         <div class="logo4">
+         <img src="css/whatsapp-logo-png-hd-2.png" />
+         </div>-->
+   
+  	  </div>
+   </footer>
+</body>
+</html>
